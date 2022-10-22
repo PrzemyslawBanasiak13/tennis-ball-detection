77b3e36fa2cdde7f94fbee73fe3f86eedf582f0b
el-banano13 <przemyslaw_banasiak02@wp.pl>
2022-10-22 16:27:27 +0200

    notebooks\helper_functions.py: add function for printing image info

--- notebooks/helper_functions.py
+++ notebooks/helper_functions.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import os
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 
 def plot_images(image_folder="data", img_count=4):
     """
-    Plot <img_count> random images from <folder_name> from subfolders
+    Plot <img_count> random images from <folder_name> from subfolders.
     """
     all_images = []
     axes = []
@@ -30,7 +32,7 @@ def plot_images(image_folder="data", img_count=4):
 
 def plot_losses(history):
     """
-    Plot losses from <history> generated during training
+    Plot losses from <history> generated during training.
     """
     fig = plt.figure()
     fig.set_size_inches(18.5, 10.5)
@@ -41,3 +43,39 @@ def plot_losses(history):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
+
+
+def print_image_info(image_path):
+    """
+    Prints informations about image from <image_path>.
+    """
+    # read the image data using PIL
+    image = Image.open(image_path)
+
+    # extract other basic metadata
+    info_dict = {
+    "Filename": image.filename,
+    "Image Size": image.size,
+    "Image Height": image.height,
+    "Image Width": image.width,
+    "Image Format": image.format,
+    "Image Mode": image.mode,
+    "Image is Animated": getattr(image, "is_animated", False),
+    "Frames in Image": getattr(image, "n_frames", 1)
+    }
+
+    for label,value in info_dict.items():
+        print(f"{label:25}: {value}")
+
+    # extract EXIF data
+    exifdata = image.getexif()
+
+    # iterating over all EXIF data fields
+    for tag_id in exifdata:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}: {data}")
